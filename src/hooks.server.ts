ba6d545581f3b6f6a0d8398288584749062f0635
Alfred Chibuike Theophane <chibyk089@gmail.com>
2024-01-21 04:55:54 +0100

    hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,1 @@
-import { auth } from '$lib/server/lucia';
-import type { Handle } from '@sveltejs/kit';
 
-export const handle: Handle = async ({ event, resolve }) => {
-	event.locals.auth = auth.handleRequest(event);
-	return await resolve(event);
-};
